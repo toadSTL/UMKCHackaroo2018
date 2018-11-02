a4e2c9e29349d9090fe87e64009006c113f6ae4f
toadSTL <gregory.dylan.brown@gmail.com>
2018-11-02 14:57:17 -0500

    loading in data from csv

---
+++ FinanceAI/build.sbt
@@ -0,0 +1,10 @@
+name := "FinanceAI"
+
+version := "0.1"
+
+scalaVersion := "2.12.7"
+
+// https://mvnrepository.com/artifact/edu.stanford.nlp/stanford-corenlp
+libraryDependencies += "edu.stanford.nlp" % "stanford-corenlp" % "3.9.2"
+// https://mvnrepository.com/artifact/com.opencsv/opencsv
+libraryDependencies += "com.opencsv" % "opencsv" % "4.3.2"
--- FinanceAI/src/main/java/test.java
+++ FinanceAI/src/main/java/test.java
@@ -1,4 +1,118 @@
-package PACKAGE_NAME;
+// two CSV files
+// with different seperators 
+
+import java.io.FileReader;
+import java.util.List;
+import com.opencsv.*;
 
 public class test {
-}
+    private static final String CSV_FILE_PATH
+            = "data/Combined_News_DJIA.csv";
+    private static final String CSV_FILE_CUSTOM_SEPERATOR
+            = "D:\\EclipseWorkSpace\\CSVOperations\\results_semicolon_Seperator.csv";
+
+    public static void main(String[] args)
+    {
+
+        System.out.println("Read Data Line by Line With Header \n");
+        readDataLineByLine(CSV_FILE_PATH);
+        System.out.println("_______________________________________________");
+
+        System.out.println("Read All Data at Once and Hide the Header also \n");
+        readAllDataAtOnce(CSV_FILE_PATH);
+        System.out.println("_______________________________________________");
+        /*
+        System.out.println("Custom Seperator here semi-colon\n");
+        readDataFromCustomSeperator(CSV_FILE_CUSTOM_SEPERATOR);
+        System.out.println("_______________________________________________");
+        */
+    }
+
+    public static void readDataLineByLine(String file)
+    {
+
+        try {
+
+            // Create an object of filereader class 
+            // with CSV file as a parameter. 
+            FileReader filereader = new FileReader(file);
+
+            // create csvReader object passing 
+            // filereader as parameter 
+            CSVReader csvReader = new CSVReader(filereader);
+            String[] nextRecord;
+
+            // we are going to read data line by line 
+            while ((nextRecord = csvReader.readNext()) != null) {
+                for (String cell : nextRecord) {
+                    System.out.print(cell + "\t");
+                }
+                System.out.println();
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void readAllDataAtOnce(String file)
+    {
+        try {
+
+            // Create an object of filereader class 
+            // with CSV file as a parameter. 
+            FileReader filereader = new FileReader(file);
+
+            // create csvReader object 
+            // and skip first Line 
+            CSVReader csvReader = new CSVReaderBuilder(filereader)
+                    .withSkipLines(1)
+                    .build();
+            List<String[]> allData = csvReader.readAll();
+
+            // print Data 
+            for (String[] row : allData) {
+                for (String cell : row) {
+                    System.out.print(cell + "\t");
+                }
+                System.out.println();
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void readDataFromCustomSeperator(String file)
+    {
+        try {
+            // Create object of filereader 
+            // class with csv file as parameter. 
+            FileReader filereader = new FileReader(file);
+
+            // create csvParser object with 
+            // custom seperator semi-colon 
+            CSVParser parser = new CSVParserBuilder().withSeparator(';').build();
+
+            // create csvReader object with 
+            // parameter filereader and parser 
+            CSVReader csvReader = new CSVReaderBuilder(filereader)
+                    .withCSVParser(parser)
+                    .build();
+
+            // Read all data at once 
+            List<String[]> allData = csvReader.readAll();
+
+            // print Data 
+            for (String[] row : allData) {
+                for (String cell : row) {
+                    System.out.print(cell + "\t");
+                }
+                System.out.println();
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+} 
